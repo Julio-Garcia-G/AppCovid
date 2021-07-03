481f4a94330f8cb99cc3556625bfe6a3c378ef2a
EduardoGA7 <juan.gonzalez.151.cb84@gmail.com>
2021-07-02 21:42:12 -0500

    Botones de redireccionamiento

--- app/src/main/java/com/example/appcovidlapiedad/DetallesHospital.java
+++ app/src/main/java/com/example/appcovidlapiedad/DetallesHospital.java
@@ -1,10 +1,14 @@
 package com.example.appcovidlapiedad;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.appcovidlapiedad.fragments.ListadoFragment;
 import com.example.appcovidlapiedad.tablas.Ocupacion_hospitales;
 import com.example.appcovidlapiedad.tablas.detalles_hospitales;
 
@@ -13,6 +17,7 @@ public class DetallesHospital extends AppCompatActivity {
     private TextView nombre, telefono, domicilio;
     private detalles_hospitales detalles;
     private Ocupacion_hospitales nombreHospital;
+    private Button regresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,17 @@ public class DetallesHospital extends AppCompatActivity {
         nombre = (TextView) findViewById((R.id.campoNombre));
         telefono = (TextView) findViewById(R.id.campoTelefono);
         domicilio = (TextView) findViewById(R.id.campoDomicilio);
+        regresar = (Button) findViewById(R.id.boton_regresar);
+
+        regresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(DetallesHospital.this,MenuLateralActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
         initValues();
     }
