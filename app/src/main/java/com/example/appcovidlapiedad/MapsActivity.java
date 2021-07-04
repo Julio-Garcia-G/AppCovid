91eedb4d577e9a2e65f45f0e4ce40f491c1e87bd
Julio-Garcia-G <julio.garcia.386.cb84@gmail.com>
2021-07-03 20:09:08 -0500

    Inclusión de API Key y modificación en MapsActivity

--- app/src/main/java/com/example/appcovidlapiedad/MapsActivity.java
+++ app/src/main/java/com/example/appcovidlapiedad/MapsActivity.java
@@ -44,8 +44,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng hospital1 = new LatLng(20.3400049, -102.0241944);
+        mMap.addMarker(new MarkerOptions().position(hospital1).title("Hospital Santa María"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hospital1,12));
     }
 }
